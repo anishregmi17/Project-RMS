f1ff73a747afa4e91eba87b394a361462741bfa4
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-03 19:06:07 +0545

    add categories

--- resources/views/management/category.blade.php
+++ resources/views/management/category.blade.php
@@ -5,14 +5,21 @@
     <div class="row justify-content-center">
       <div class="col-md-4">
         <div class="list-group">
-            <a href="" class="list-group-item">Category</a>
-            <a href="" class="list-group-item">Category</a>
-            <a href="" class="list-group-item">Category</a>
-            <a href="" class="list-group-item">Categoerrrrry</a>
+            <a href="/management/category" class="list-group-item list-group-item-action">
+                <i class="fa-solid fa-list"></i> Category</a>
+            <a href="/management/menu" class="list-group-item list-group-item-action">
+                <i class="fa-solid fa-caret-down"></i> Menu</a>
+            <a href="/management/table" class="list-group-item list-group-item-action">
+                <i class="fa-solid fa-table"></i> Table</a>
+            <a href="/management/user" class="list-group-item list-group-item-action">
+                <i class="fa-solid fa-user"></i> User</a>
         </div>
       </div>
       <div class="col-md-8">
-        Content
+        <i class="fa-solid fa-list"></i>
+        Category
+        <a href="/management/category/create" class="btn btn-success btn-sm float-right"><i class="fa-solid fa-plus"></i>Create Category</a>
+        <hr>
     </div>
     </div>
   </div>
--- resources/views/management/createCategory.blade.php
+++ resources/views/management/createCategory.blade.php
@@ -16,8 +16,19 @@
         </div>
       </div>
       <div class="col-md-8">
-        Content
+        <i class="fa-solid fa-list"></i>
+       Create Category
+        <hr>
+        <form action="/management/category" method="POST">
+            @csrf
+            <div class="form-group">
+              <label for="categoryName">Category Name</label>
+              <input type="text" name="name" class="form-control" placeholder="Category...">
+            </div>
+            <button type="submit" class="btn btn-primary">Save</button>
+          </form>
     </div>
     </div>
   </div>
 @endsection
+
