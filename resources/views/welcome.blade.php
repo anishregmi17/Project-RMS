5794e1bc478dd906e674b3fe377a453f72f05901
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-12 18:23:04 +0545

    modify welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,20 +13,27 @@
         <!-- Styles -->
         <style>
             /* Tailwind CSS v3.2.4 | MIT License | https://tailwindcss.com */
+            body {
+                font-family: 'Figtree', sans-serif;
+                background-color: #c4c3c3;
+                margin: 0;
+                padding: 0;
+            }
             .navbar {
                 display: flex;
                 justify-content: space-between;
                 align-items: center;
                 padding: 1em;
-                background-color: #594a40;
-                color: #fff;
+                background-color: #ffffff;
+                color: #594a40;
                 position: fixed;
                 width: 100%;
                 top: 0;
                 z-index: 1000;
+                box-shadow: 0 2px 4px rgba(0,0,0,0.1);
             }
             .navbar a {
-                color: #fff;
+                color: #594a40;
                 text-decoration: none;
                 margin: 0 1em;
             }
@@ -37,7 +44,7 @@
                 text-align: center;
                 min-height: 80vh;
                 background: url('https://source.unsplash.com/1600x900/?restaurant') no-repeat center center/cover;
-                color: #fff;
+                color: #bf6969;
                 font-family: 'Figtree', sans-serif;
                 padding: 2em;
                 margin-top: 60px;
@@ -51,14 +58,30 @@
                 margin-top: 1em;
                 text-shadow: 1px 1px 3px #000;
             }
+
+            .content h2 {
+                color: #594a40;
+                text-align: center;
+                margin-bottom: 1em;
+            }
+            .content p {
+                color: #594a40;
+                margin-bottom: 1em;
+            }
+            .content ul {
+                list-style-type: disc;
+                padding-left: 20px;
+                color: #594a40;
+            }
             .footer {
                 text-align: center;
                 padding: 1em;
-                background-color: #594a40;
-                color: #fff;
+                background-color: #ffffff;
+                color: #594a40;
                 position: fixed;
                 width: 100%;
                 bottom: 0;
+                box-shadow: 0 -2px 4px rgba(0,0,0,0.1);
             }
         </style>
     </head>
@@ -72,10 +95,9 @@
                     @auth
                         <a href="{{ url('/home') }}">Enter to Management System</a>
                     @else
-                        <a href="{{ route('login') }}">Log in</a>
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4">Register</a>
-                        @endif
+                    <a href="{{ route('login') }}" style="padding: 10px 20px; background-color: #594a40; color: #ffffff; border-radius: 5px; text-decoration: none;">Log in</a>
+                    @if (Route::has('register'))
+                    <a href="{{ route('register') }}" style="padding: 10px 20px; background-color: #fbbd61; color: #ffffff; border-radius: 5px; text-decoration: none; margin-left: 10px;">Register</a>                        @endif
                     @endauth
                 @endif
             </div>
@@ -87,6 +109,7 @@
                 <p>Experience the best dining with our top-notch services and gourmet dishes.</p>
             </div>
         </div>
+
 
         <div class="footer">
             &copy; 2024 PNC Food Park. All rights reserved.
